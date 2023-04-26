196e75d013825f1b3c59d8077f0fdd58ffdb585a
matheusinit <matheus.oliveira.s@protonmail.com>
2023-04-26 15:58:34 -0300

    fix: add repository dependency to usecase

--- src/infra/http/routes/user.ts
+++ src/infra/http/routes/user.ts
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router, type Request, type Response } from 'express'
 import { RegisterUser } from '../../../usecases/register-user'
+import { PrismaUserRepository } from '~/data/repositories/prisma/prisma-user-repository'
 
 const userRoutes = Router()
 
 userRoutes.post('/', async (request: Request, response: Response) => {
   const { name, email, password } = request.body
 
-  const registerUser = new RegisterUser()
+  const userRepository = new PrismaUserRepository()
+  const registerUser = new RegisterUser(userRepository)
 
   const user = await registerUser.execute({ name, email, password })
 
