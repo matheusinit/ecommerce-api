5528eb06580a4875c771b4f166cf9964b71b1425
matheusinit <matheus.oliveira.s@protonmail.com>
2023-04-27 22:35:28 -0300

    test: ensure register user should throw if name is not 3 characters long at least

--- src/usecases/register-user.spec.ts
+++ src/usecases/register-user.spec.ts
@@ -1,5 +1,4 @@
 // 8. Ensure a user logged in cannot create an account
-// 9. Ensure that email is a valid email
 // 10. Check lengths for name as name and surname
 // 11. Check password to be at least 8 chars long and use letter and numbers with special chars
 // 12. Check for fields to be empty (ensure that throw if they are, except name)
@@ -130,6 +129,21 @@ describe('Register user usecase', () => {
     const userData = {
       name: 'Matheus Oliveira',
       email: 'invalid_email',
+      password: 'some-random-password',
+      type: 'CUSTOMER' as const
+    }
+
+    const promise = sut.execute(userData)
+
+    void expect(promise).rejects.toThrowError()
+  })
+
+  it('should throw if name is not 3 chars long at least', async () => {
+    const { sut } = makeSut()
+
+    const userData = {
+      name: 'Ab',
+      email: 'valid_email@email.com',
       password: 'some-random-password',
       type: 'CUSTOMER' as const
     }
