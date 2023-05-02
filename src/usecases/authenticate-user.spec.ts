3586997488fba3c3f31e5231af8b5fb14f75952f
matheusinit <matheus.oliveira.s@protonmail.com>
2023-05-02 08:47:44 -0300

    refactor: create factory function to creates sut

--- src/usecases/authenticate-user.spec.ts
+++ src/usecases/authenticate-user.spec.ts
@@ -1,12 +1,21 @@
 import { expect, it, describe } from 'vitest'
 import { AuthenticateUser } from './authenticate-user'
 
+const makeSut = () => {
+  const sut = new AuthenticateUser()
+
+  return {
+    sut
+  }
+}
+
 describe('Authenticate User', () => {
   it('should throw if email is not provided', async () => {
+    const { sut } = makeSut()
+
     const userCredentails = {
       password: 'random-password'
     }
-    const sut = new AuthenticateUser()
 
     // @ts-expect-error "This should not use email property to test feature in run time instead of compile time"
     const promise = sut.execute(userCredentails)
@@ -15,7 +24,7 @@ describe('Authenticate User', () => {
   })
 
   it('should throw if password is not provided', async () => {
-    const sut = new AuthenticateUser()
+    const { sut } = makeSut()
 
     const userCredentails = {
       email: 'user@email.com'
