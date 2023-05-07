7794fcf4b6019ef68296ccf50e3d855d82696349
matheusinit <matheus.oliveira.s@protonmail.com>
2023-05-06 22:23:11 -0300

    refactor: use express route adapt and create factory function for authenticate user controller

--- src/infra/http/routes/auth.ts
+++ src/infra/http/routes/auth.ts
@@ -1,17 +1,20 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { Router, type Request, type Response } from 'express'
+import { Router } from 'express'
 import { PrismaUserRepository } from '~/data/repositories/prisma/prisma-user-repository'
 import { AuthenticateUserController } from '~/controllers/authenticate-user-controller'
 import { AuthenticateUser } from '~/usecases/authenticate-user'
+import { expressRouteAdapt } from '~/utils/express-route-adapt'
 
-const authRoutes = Router()
-
-authRoutes.post('/', async (request: Request, response: Response) => {
+const makeAuthenticateUserController = () => {
   const userRepository = new PrismaUserRepository()
   const authenticateUser = new AuthenticateUser(userRepository)
   const authenticateUserController = new AuthenticateUserController(authenticateUser)
 
-  return await authenticateUserController.handle(request, response)
-})
+  return authenticateUserController
+}
+
+const authRoutes = Router()
+
+authRoutes.post('/', expressRouteAdapt(makeAuthenticateUserController()))
 
 export default authRoutes
