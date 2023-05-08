28f14fea4e8cd0cf349266aba84b110a44d4672a
matheusinit <matheus.oliveira.s@protonmail.com>
2023-05-08 16:43:23 -0300

    refactor: move validation logic into a another validation.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,47 +1,11 @@
 // config/env.ts
 import * as z from 'zod'
 
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
-const isProduction = process.env.NODE_ENV === 'production'
-const isDevelopment = process.env.NODE_ENV === 'development'
-const isTest = process.env.NODE_ENV === 'test'
-
-// -------------------
-// Base env schema
-// -------------------
-const baseSchema = z
-  .object({
-    production: z.boolean(),
-    development: z.boolean(),
-    test: z.boolean()
-  })
-  .nonstrict()
-
-// ----------------------
-// Production env schema
-// ----------------------
-const productionEnvSchema = z.object({
+export const productionEnvSchema = z.object({
   ACCESS_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
   PORT: z.number(),
   REDIS_PASSWORD: z.string(),
   REDIS_HOST: z.string(),
   REDIS_PORT: z.string()
-})
-
-let envSchema
-if (isProduction) {
-  envSchema = baseSchema.merge(productionEnvSchema)
-} else {
-  envSchema = baseSchema.merge(productionEnvSchema.partial())
-}
-
-export const env = envSchema.parse({
-  ...process.env,
-  production: isProduction,
-  development: isDevelopment,
-  test: isTest
 })
---
+++ src/config/validator.ts
@@ -0,0 +1,34 @@
+import * as z from 'zod'
+import * as dotenv from 'dotenv'
+import { productionEnvSchema } from './env'
+
+dotenv.config()
+
+const isProduction = process.env.NODE_ENV === 'production'
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isTest = process.env.NODE_ENV === 'test'
+
+// -------------------
+// Base env schema
+// -------------------
+const baseSchema = z
+  .object({
+    production: z.boolean(),
+    development: z.boolean(),
+    test: z.boolean()
+  })
+  .nonstrict()
+
+let envSchema
+if (isProduction) {
+  envSchema = baseSchema.merge(productionEnvSchema)
+} else {
+  envSchema = baseSchema.merge(productionEnvSchema.partial())
+}
+
+export const env = envSchema.parse({
+  ...process.env,
+  production: isProduction,
+  development: isDevelopment,
+  test: isTest
+})
