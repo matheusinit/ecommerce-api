2feeb4d7b38e4f39a34415923ab05eb478ae1857
matheusinit <matheus.oliveira.s@protonmail.com>
2023-05-07 23:11:59 -0300

    test: ensure logout user should return success as true when a valid token is passed

--- src/usecases/logout-user.spec.ts
+++ src/usecases/logout-user.spec.ts
@@ -5,11 +5,32 @@ import { InMemoryTokenRepository } from '~/data/repositories/in-memory/in-memory
 describe('Logout user', () => {
   it('should throw an error if the token is invalid', async () => {
     const inMemoryTokenRepository = new InMemoryTokenRepository()
-    const sut = new LogoutUser(inMemoryTokenRepository)
+    const verifyTokenMocked = async (token: string, secret?: string) => {
+      return await new Promise((resolve, reject) => { reject(new Error()) })
+    }
+    const sut = new LogoutUser(inMemoryTokenRepository, verifyTokenMocked)
     const accessToken = 'invalid-token'
 
     const result = await sut.execute({ accessToken })
 
     expect(result.sucess).toBe(false)
+  })
+
+  it('should return success as true when a valid token is passed', async () => {
+    const inMemoryTokenRepository = new InMemoryTokenRepository()
+    const verifyTokenMocked = async (token: string, secret?: string) => {
+      return await new Promise((resolve, reject) => {
+        resolve({
+          id: 'user-id',
+          type: 'user-type'
+        })
+      })
+    }
+    const sut = new LogoutUser(inMemoryTokenRepository, verifyTokenMocked)
+    const accessToken = 'valid-token'
+
+    const result = await sut.execute({ accessToken })
+
+    expect(result.sucess).toBe(true)
   })
 })
