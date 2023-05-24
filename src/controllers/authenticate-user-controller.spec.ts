2a6ccafd6127723749fe02a7b13854106c8607d4
matheusinit <matheus.oliveira.s@protonmail.com>
2023-05-23 22:13:10 -0300

    test: ensure authenticate user controller should return a bad request if email is not provided

---
+++ src/controllers/authenticate-user-controller.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest'
+import { AuthenticateUserController } from './authenticate-user-controller'
+import { type AuthenticateUser } from '~/data/protocols/authenticate-user'
+import { type HttpRequest } from '~/infra/protocols/http-request'
+
+describe('Authenticate user controller', () => {
+  it('should return a bad request if email is not provided', async () => {
+    class AuthenticateUserStub implements AuthenticateUser {
+      async execute (request: { email: string, password: string }) {
+        return {
+          accessToken: 'random-token',
+          refreshToken: 'random-token'
+        }
+      }
+    }
+
+    const usecase = new AuthenticateUserStub()
+
+    const sut = new AuthenticateUserController(usecase)
+
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'random-password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.body).toEqual({ message: 'email is required' })
+  })
+})
