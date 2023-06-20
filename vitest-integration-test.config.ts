1647b7683c6d0f13c54f7fdadc0540dae4cf7b65
matheusinit <matheus.oliveira.s@protonmail.com>
2023-06-20 08:43:11 -0300

    chore: configure script to run integration tests and unit tests separately

--- vitest-integration-test.config.ts
+++ vitest-integration-test.config.ts
@@ -4,5 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    include: [
+      '**/__tests__/**/*.?(c|m)[jt]s?(x)',
+      '**/?(*.){test,}.?(c|m)[jt]s?(x)'
+    ]
   }
 })
