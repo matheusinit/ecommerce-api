265ccffc92186d8e31cd2567de5c81b0682a09f4
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-11 09:19:02 -0300

    fix: use correct path to get controller and usecase classes

--- src/infra/http/routes/product.ts
+++ src/infra/http/routes/product.ts
@@ -1,22 +1,22 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import { CreateProductController } from '~/controllers/create-product-controller'
+import { CreateProductController } from '~/controllers/publish-product-controller'
 import { ListProductsController } from '~/controllers/list-products-controller'
 import { PrismaProductRepository } from '~/data/repositories/prisma/prisma-product-repository'
 import { PrismaUserRepository } from '~/data/repositories/prisma/prisma-user-repository'
-import { DbCreateProduct } from '~/usecases/create-product'
+import { DbPublishProduct } from '~/usecases/db-publish-product'
 import { ListProducts } from '~/usecases/list-products'
 import { expressRouteAdapt } from '~/utils/express-route-adapt'
 
 const productRoutes = Router()
 
-const makeCreateProductController = () => {
+const makePublishProductController = () => {
   const userRepository = new PrismaUserRepository()
   const productRepository = new PrismaProductRepository()
-  const createProduct = new DbCreateProduct(productRepository, userRepository)
-  const createProductController = new CreateProductController(createProduct)
+  const dbPublishProduct = new DbPublishProduct(productRepository, userRepository)
+  const publishProductController = new CreateProductController(dbPublishProduct)
 
-  return createProductController
+  return publishProductController
 }
 
 const makeListProductsController = () => {
@@ -25,6 +25,6 @@ const makeListProductsController = () => {
 }
 
 productRoutes.get('/', expressRouteAdapt(makeListProductsController()))
-productRoutes.post('/', expressRouteAdapt(makeCreateProductController()))
+productRoutes.post('/', expressRouteAdapt(makePublishProductController()))
 
 export default productRoutes
