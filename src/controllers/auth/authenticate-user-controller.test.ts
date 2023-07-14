08116fe12e28f41aca9be3528321270ca2f76fd9
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-13 23:07:28 -0300

    test: ensure POST /auth get a bad request when providing invalid email

--- src/controllers/auth/authenticate-user-controller.test.ts
+++ src/controllers/auth/authenticate-user-controller.test.ts
@@ -131,5 +131,28 @@ describe('POST /auth', () => {
       expect(response.statusCode).toBe(400)
       expect(response.body).toBeDefined()
     })
+
+    it('when providing invalid email, then should get bad request', async () => {
+      const user: User = {
+        name: 'Matheus Oliveira',
+        type: 'STORE-ADMIN',
+        email: 'matheus.oliveira@email.com',
+        password: 'minhasenha1!'
+      }
+
+      await request(app)
+        .post('/v1/user')
+        .send(user)
+
+      const response = await request(app)
+        .post('/v1/auth')
+        .send({
+          email: 'invalid-email@email.com',
+          password: user.password
+        })
+
+      expect(response.statusCode).toBe(400)
+      expect(response.body).toBeDefined()
+    })
   })
 })
