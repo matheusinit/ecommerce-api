4ef6d92e5e1e2cef9e03832f6ff9ee8fc5466266
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-20 19:15:00 -0300

    refactor: use endpoint and http method to describe test case

--- src/controllers/product/list-products-controller.test.ts
+++ src/controllers/product/list-products-controller.test.ts
@@ -2,41 +2,80 @@ import { beforeAll, afterEach, describe, expect, it, afterAll } from 'vitest'
 import request from 'supertest'
 import app from '~/app'
 import { PrismaClient } from '@prisma/client'
+import { type UserType } from '~/data/dtos/user-type'
 
 let prisma: PrismaClient
 
-describe('List products controller', () => {
+// describe('POST /auth', () => {
+//   beforeAll(async () => {
+//     prisma = new PrismaClient()
+//
+//     await prisma.$connect()
+//   })
+//
+//   afterEach(async () => {
+//     await prisma.user.delete({
+//       where: {
+//         email: 'matheus.oliveira@email.com'
+//       }
+//     })
+//   })
+//
+//   afterAll(async () => {
+//     await prisma.$disconnect()
+//   })
+//
+//   describe('when adding a valid user and body', () => {
+//     it('then should authenticate user', async () => {
+//       const user: User = {
+//         name: 'Matheus Oliveira',
+//         type: 'STORE-ADMIN',
+//         email: 'matheus.oliveira@email.com',
+//         password: 'minhasenha1!'
+//       }
+//
+//       await request(app)
+//         .post('/v1/user')
+//         .send(user)
+//
+//       const response = await request(app)
+//         .post('/v1/auth')
+//         .send({
+//           email: user.email,
+//           password: user.password
+//         })
+//
+//       expect(response.statusCode).toBe(200)
+//       expect(response.body).toBeDefined()
+//     })
+
+interface User {
+  name: string
+  type: UserType
+  email: string
+  password: string
+}
+
+describe('GET /product', () => {
   beforeAll(async () => {
     prisma = new PrismaClient()
 
     await prisma.$connect()
 
-    await request(app).post('/v1/user').send({
-      name: 'Tester',
-      email: 'tester@email.com',
+    const user: User = {
+      name: 'Matheus Oliveira',
       type: 'STORE-ADMIN',
-      password: 'testerpass13!'
-    })
+      email: 'matheus.oliveira@email.com',
+      password: 'minhasenha1!'
+    }
+
+    await request(app).post('/v1/user').send(user)
   })
 
   afterEach(async () => {
-    const product = await prisma.product.findFirst({
-      where: {
-        name: 'Teclado Mecânico com fio Logitech K835 TKL com Estrutura de Alumínio e Switch Red Linear'
-      }
-    })
+    await prisma.product.deleteMany()
 
-    await prisma.product.delete({
-      where: {
-        id: product?.id ?? ''
-      }
-    })
-
-    await prisma.user.delete({
-      where: {
-        email: 'tester@email.com'
-      }
-    })
+    await prisma.user.deleteMany()
   })
 
   afterAll(async () => {
@@ -52,13 +91,13 @@ describe('List products controller', () => {
     const { body } = await request(app)
       .post('/v1/auth')
       .send({
-        email: 'tester@email.com',
-        password: 'testerpass13!'
+        email: 'matheus.oliveira@email.com',
+        password: 'minhasenha1!'
       })
 
     const tokens: Tokens = body
 
-    await request.agent(app)
+    await request(app)
       .post('/v1/product')
       .set('Cookie', [`access-token=${tokens.accessToken}`, `refresh-token=${tokens.refreshToken}`])
       .send({
