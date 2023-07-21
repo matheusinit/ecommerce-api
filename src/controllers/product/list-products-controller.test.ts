b896007a2cb71644286c9bcfa90bd1d1a2639755
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-20 22:07:59 -0300

    chore: remove comments

--- src/controllers/product/list-products-controller.test.ts
+++ src/controllers/product/list-products-controller.test.ts
@@ -6,49 +6,6 @@ import { type UserType } from '~/data/dtos/user-type'
 
 let prisma: PrismaClient
 
-// describe('POST /auth', () => {
-//   beforeAll(async () => {
-//     prisma = new PrismaClient()
-//
-//     await prisma.$connect()
-//   })
-//
-//   afterEach(async () => {
-//     await prisma.user.delete({
-//       where: {
-//         email: 'matheus.oliveira@email.com'
-//       }
-//     })
-//   })
-//
-//   afterAll(async () => {
-//     await prisma.$disconnect()
-//   })
-//
-//   describe('when adding a valid user and body', () => {
-//     it('then should authenticate user', async () => {
-//       const user: User = {
-//         name: 'Matheus Oliveira',
-//         type: 'STORE-ADMIN',
-//         email: 'matheus.oliveira@email.com',
-//         password: 'minhasenha1!'
-//       }
-//
-//       await request(app)
-//         .post('/v1/user')
-//         .send(user)
-//
-//       const response = await request(app)
-//         .post('/v1/auth')
-//         .send({
-//           email: user.email,
-//           password: user.password
-//         })
-//
-//       expect(response.statusCode).toBe(200)
-//       expect(response.body).toBeDefined()
-//     })
-
 interface User {
   name: string
   type: UserType
