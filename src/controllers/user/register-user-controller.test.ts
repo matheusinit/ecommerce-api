58e73232181937a8acbe0a451d576a8ac0e2dc0a
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-24 09:38:00 -0300

    test: ensure POST /users should get bad request when provided password has non-special characters and one number

--- src/controllers/user/register-user-controller.test.ts
+++ src/controllers/user/register-user-controller.test.ts
@@ -162,5 +162,19 @@ describe('POST /users', () => {
       expect(response.status).toBe(400)
       expect(response.body.message).toBeDefined()
     })
+
+    it('when provided password is has non-special characters e one number, should get bad request', async () => {
+      const user: User = {
+        name: 'Matheus Oliveira',
+        type: 'STORE-ADMIN',
+        email: 'matheus.oliveira@email.com',
+        password: 'password1'
+      }
+
+      const response = await request(app).post('/v1/users').send(user)
+
+      expect(response.status).toBe(400)
+      expect(response.body.message).toBeDefined()
+    })
   })
 })
