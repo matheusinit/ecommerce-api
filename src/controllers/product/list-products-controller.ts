8a95a1b7e14828e2962b53cb9da593f0fe633e9e
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-30 15:28:59 -0300

    refactor: wrap in try catch to return bad request when per page is passed as 0

--- src/controllers/product/list-products-controller.ts
+++ src/controllers/product/list-products-controller.ts
@@ -2,7 +2,7 @@ import { type Controller } from '~/infra/protocols/controller'
 import { type HttpRequest } from '~/infra/protocols/http-request'
 import { validatePaginationQueryParams } from '~/lib/pagination'
 import { type ListProducts } from '~/usecases/list-products'
-import { badRequest, httpError, ok } from '~/utils/http'
+import { badRequest, httpError, internalServerError, ok } from '~/utils/http'
 import { defineResponseHeader } from '~/utils/response-headers'
 
 export class ListProductsController implements Controller {
@@ -11,28 +11,34 @@ export class ListProductsController implements Controller {
   ) {}
 
   async handle (request: HttpRequest) {
-    const perPageQuery = request.query?.per_page
-    const pageQuery = request.query?.page
+    try {
+      const perPageQuery = request.query?.per_page
+      const pageQuery = request.query?.page
 
-    if (perPageQuery === '0') {
-      return badRequest(httpError('per_page has to be 1 or greater'))
+      const { perPage, page } = validatePaginationQueryParams({ perPage: perPageQuery, page: pageQuery })
+
+      const skipCount = page * perPage
+      const getCount = perPage
+
+      const products = await this.listProducts.execute({
+        skipCount,
+        getCount
+      })
+
+      const response = ok(products)
+
+      return defineResponseHeader(response, {
+        'Pagination-Page-Count': '2',
+        'Pagination-Total-Count': '10'
+      })
+    } catch (err) {
+      const error = err as Error
+
+      if (error.message === 'per_page has to be 1 or greater') {
+        return badRequest(httpError(error.message))
+      }
+
+      return internalServerError('unexpect error occured')
     }
-
-    const { perPage, page } = validatePaginationQueryParams({ perPage: perPageQuery, page: pageQuery })
-
-    const skipCount = page * perPage
-    const getCount = perPage
-
-    const products = await this.listProducts.execute({
-      skipCount,
-      getCount
-    })
-
-    const response = ok(products)
-
-    return defineResponseHeader(response, {
-      'Pagination-Page-Count': '2',
-      'Pagination-Total-Count': '10'
-    })
   }
 }
