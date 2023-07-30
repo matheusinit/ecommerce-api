27446d4290e5fe96b3c1a5df5e1b812db4b5a44d
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-30 14:59:39 -0300

    test: ensure pagination get query params in number type when query params is provided

--- src/lib/pagination/pagination.spec.ts
+++ src/lib/pagination/pagination.spec.ts
@@ -13,5 +13,19 @@ describe('Pagination', () => {
 
       expect(queryParams).toBeDefined()
     })
+
+    it('when query params is provided, get query params in number type', async () => {
+      const page = '0'
+      const perPage = '10'
+
+      const queryParams = validatePaginationQueryParams({
+        page, perPage
+      })
+
+      expect(queryParams).toMatchObject({
+        page: 0,
+        perPage: 10
+      })
+    })
   })
 })
