fbba2f96fbf41a419962ef2fe7e80057e906a507
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-13 22:44:40 -0300

    feat: ensure unauthorized is returned when jwt token is malformed

--- src/infra/http/middlewares/auth.ts
+++ src/infra/http/middlewares/auth.ts
@@ -6,19 +6,29 @@ import { verifyToken } from '~/usecases/verify-token'
 const tokenRepository = new RedisTokenRepository()
 
 export const isAuthenticated = async (request: Request, response: Response, next: NextFunction) => {
-  const accessToken = request.cookies['access-token']
+  try {
+    const accessToken = request.cookies['access-token']
 
-  if (!accessToken ?? !accessToken.trim()) {
-    return response.status(401).send({ message: 'Not authenticated' })
+    if (!accessToken ?? !accessToken.trim()) {
+      return response.status(401).send({ message: 'Not authenticated' })
+    }
+
+    const payload = await verifyToken(accessToken, env.ACCESS_TOKEN_SECRET)
+
+    const isTokenBlackListed = await tokenRepository.getBykey(payload.id)
+
+    if (isTokenBlackListed) {
+      return response.status(401).send({ message: 'Not authenticated' })
+    }
+
+    next()
+  } catch (err) {
+    const error = err as Error
+
+    if (error.message === 'The token is malformed.') {
+      return response.status(401).send({ message: 'Not authenticated' })
+    }
+
+    return response.status(500).send({ message: 'Unexpected error occured' })
   }
-
-  const payload = await verifyToken(accessToken, env.ACCESS_TOKEN_SECRET)
-
-  const isTokenBlackListed = await tokenRepository.getBykey(payload.id)
-
-  if (isTokenBlackListed) {
-    return response.status(401).send({ message: 'Not authenticated' })
-  }
-
-  next()
 }
