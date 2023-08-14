45d738cb2ce6558e5a9c54c766db2ad54736080a
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-13 22:43:37 -0300

    chore: add type for jwt payload

--- src/usecases/verify-token.ts
+++ src/usecases/verify-token.ts
@@ -1,9 +1,17 @@
 import { createVerifier } from 'fast-jwt'
+import { type UserType } from '~/data/dtos/user-type'
+
+interface JwtPayload {
+  id: string
+  type: UserType
+  iat: number
+  exp: number
+}
 
 export const verifyToken = async (token: string, secret?: string) => {
   const verifierAsync = createVerifier({ key: async () => secret })
 
-  const result = await verifierAsync(token)
+  const result: JwtPayload = await verifierAsync(token)
 
   return result
 }
