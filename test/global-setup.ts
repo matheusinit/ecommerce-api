c4d36fa1be3ed2d1d603c526184c56a4fea62ffc
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-16 22:47:36 -0300

    chore: up the containers not runnning in testing setup

--- test/global-setup.ts
+++ test/global-setup.ts
@@ -1,7 +1,32 @@
 import { execSync } from 'child_process'
+import dockerCompose from 'docker-compose/dist/v2'
+import isPortReachable from 'is-port-reachable'
+import path from 'path'
 
 export const setup = async () => {
   console.log('Starting the global setup...')
+
+  const isDatabaseRunning = await isPortReachable(5440, {
+    host: '0.0.0.0'
+  })
+
+  if (!isDatabaseRunning) {
+    await dockerCompose.upOne('testing-database', {
+      cwd: path.join(__dirname),
+      log: true
+    })
+  }
+
+  const isInMemoryDatabaseRunning = await isPortReachable(6379, {
+    host: '0.0.0.0'
+  })
+
+  if (!isInMemoryDatabaseRunning) {
+    await dockerCompose.upOne('cache', {
+      cwd: path.join(__dirname),
+      log: true
+    })
+  }
 
   execSync('pnpm prisma migrate deploy')
 }
