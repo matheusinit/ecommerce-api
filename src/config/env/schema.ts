ca2438a625a157c44d1ca8bb0f49e9e4a62bd298
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-24 10:31:06 -0300

    chore: use REDIS_HOST instead of multiple environment variables

--- src/config/env/schema.ts
+++ src/config/env/schema.ts
@@ -5,8 +5,6 @@ export const productionEnvSchema = z.object({
   ACCESS_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
   PORT: z.number(),
-  REDIS_PASSWORD: z.string(),
-  REDIS_HOST: z.string(),
-  REDIS_PORT: z.string(),
+  REDIS_URL: z.string(),
   DATABASE_URL: z.string()
 })
--- src/infra/cache/index.ts
+++ src/infra/cache/index.ts
@@ -1,20 +1,25 @@
 import { createClient } from 'redis'
 import { env } from '~/config/env'
 
-if (!env.REDIS_PASSWORD) {
-  throw Error('REDIS_PASSWORD is not defined')
+if (!env.REDIS_URL) {
+  throw new Error('REDIS_URL is not defined')
 }
 
-if (!env.REDIS_HOST) {
-  throw Error('REDIS_HOST is not defined')
-}
-
-if (!env.REDIS_PORT) {
-  throw Error('REDIS_PORT is not defined')
-}
+// if (!env.REDIS_PASSWORD) {
+//   throw Error('REDIS_PASSWORD is not defined')
+// }
+//
+// if (!env.REDIS_HOST) {
+//   throw Error('REDIS_HOST is not defined')
+// }
+//
+// if (!env.REDIS_PORT) {
+//   throw Error('REDIS_PORT is not defined')
+// }
 
 const redis = createClient({
-  url: `redis://:${env.REDIS_PASSWORD}@${env.REDIS_HOST}:${env.REDIS_PORT}`
+  // url: `redis://:${env.REDIS_PASSWORD}@${env.REDIS_HOST}:${env.REDIS_PORT}`
+  url: env.REDIS_URL
 })
 
 redis.on('error', err => { console.log('Redis Client Error', err) })
