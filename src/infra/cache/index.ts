6f4057139c2eb3e5a5a79c66cbb8b66e62cda99f
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-27 08:58:45 -0300

    refactor: remove old code in comments

--- src/infra/cache/index.ts
+++ src/infra/cache/index.ts
@@ -5,20 +5,7 @@ if (!env.REDIS_URL) {
   throw new Error('REDIS_URL is not defined')
 }
 
-// if (!env.REDIS_PASSWORD) {
-//   throw Error('REDIS_PASSWORD is not defined')
-// }
-//
-// if (!env.REDIS_HOST) {
-//   throw Error('REDIS_HOST is not defined')
-// }
-//
-// if (!env.REDIS_PORT) {
-//   throw Error('REDIS_PORT is not defined')
-// }
-
 const redis = createClient({
-  // url: `redis://:${env.REDIS_PASSWORD}@${env.REDIS_HOST}:${env.REDIS_PORT}`
   url: env.REDIS_URL
 })
 
