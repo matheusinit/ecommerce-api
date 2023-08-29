e141e2b065a1804fb5fbd4c9c0fd92096da2e99a
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-28 23:14:54 -0300

    feat: ensure when a name is provided, then should change only the name of the product

--- src/usecases/edit-product.ts
+++ src/usecases/edit-product.ts
@@ -1,9 +1,22 @@
+import { type Product } from '@prisma/client'
+
+type ProductDomain = Product & {
+  stock: number
+}
+
+type ProductOptional = Partial<Omit<ProductDomain, 'id'>>
+
 export class EditProduct {
-  async execute (price?: number, name?: string, stock: number, userId: string) {
+  async execute (product: Product, changes: ProductOptional) {
+    const changesWithoutUndefined: Omit<typeof changes, 'createdAt' | 'updatedAt' | 'deletedAt'> = {}
+
+    if (changes?.name) {
+      changesWithoutUndefined.name = changes?.name
+    }
+
     return {
-      price,
-      name,
-      stock
+      ...product,
+      ...changesWithoutUndefined
     }
   }
 }
