af62ef62b61d98b4badf9e9f8a786ab37798ccbd
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-29 23:54:02 -0300

    test: ensure when a product does not exists, then should throw an error

--- src/usecases/edit-product.spec.ts
+++ src/usecases/edit-product.spec.ts
@@ -2,6 +2,18 @@ import { it, expect, describe, vitest } from 'vitest'
 import { createId as createCuid } from '@paralleldrive/cuid2'
 import { EditProduct } from './edit-product'
 import { InMemoryProductRepository } from '~/data/repositories/in-memory/in-memory-product-repository'
+import * as falso from '@ngneat/falso'
+
+const makeProduct = () => ({
+  id: falso.randUuid(),
+  name: falso.randProductName(),
+  price: falso.randNumber({ min: 1, max: 99999 }),
+  stock: falso.randNumber({ min: 0, max: 100 }),
+  userId: falso.randUuid(),
+  createdAt: falso.randPastDate(),
+  updatedAt: null,
+  deletedAt: null
+})
 
 describe('Edit product', () => {
   it('when product id is not provided, should throw an error', async () => {
@@ -21,14 +33,30 @@ describe('Edit product', () => {
     const inMemoryProductRepository = new InMemoryProductRepository()
     const sut = new EditProduct(inMemoryProductRepository)
 
+    const product = makeProduct()
+
+    void inMemoryProductRepository.create(product, product.id)
+
     const repositorySpy = vitest.spyOn(inMemoryProductRepository, 'findById')
+
+    // Act
+    await sut.execute(product.id)
+
+    // Assert
+    expect(repositorySpy).toHaveBeenCalledOnce()
+  })
+
+  it('when a product does not exists, then should throw an error', async () => {
+    // Arrange
+    const inMemoryProductRepository = new InMemoryProductRepository()
+    const sut = new EditProduct(inMemoryProductRepository)
 
     const randCuid = createCuid()
 
     // Act
-    await sut.execute(randCuid)
+    const promise = sut.execute(randCuid)
 
     // Assert
-    expect(repositorySpy).toHaveBeenCalledOnce()
+    void expect(promise).rejects.toThrow('product with given id does not exists')
   })
 })
