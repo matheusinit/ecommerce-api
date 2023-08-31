af3f701dbc26982efad53b6679b7db9fbee21b21
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-31 09:33:13 -0300

    feat: ensure when name provided is an empty string, then should get bad request

--- src/controllers/product/edit-product-controller.ts
+++ src/controllers/product/edit-product-controller.ts
@@ -14,6 +14,10 @@ export class EditProductController {
       return badRequest(httpError('Product id must be a cuid'))
     }
 
+    if (request.body.name === '') {
+      return badRequest(httpError('Name cannot be a empty string'))
+    }
+
     return badRequest(httpError('Must pass a value for fields to edit a product: name, price or stock'))
   }
 }
