c66c440fe08b7e1b2b4c3e618541e13e32d4c719
matheusinit <matheus.oliveira.s@protonmail.com>
2023-08-31 09:27:37 -0300

    feat: ensure when a product is an integer, then should get bad request

--- src/controllers/product/edit-product-controller.ts
+++ src/controllers/product/edit-product-controller.ts
@@ -1,8 +1,19 @@
 import { type HttpRequest } from '~/infra/protocols/http-request'
 import { badRequest, httpError } from '~/utils/http'
+import z from 'zod'
 
 export class EditProductController {
   async handle (request: HttpRequest) {
+    const { id } = request.params
+
+    const idValidation = z.string().cuid()
+
+    const validation = idValidation.safeParse(id)
+
+    if (!validation.success) {
+      return badRequest(httpError('Product id must be a cuid'))
+    }
+
     return badRequest(httpError('Must pass a value for fields to edit a product: name, price or stock'))
   }
 }
