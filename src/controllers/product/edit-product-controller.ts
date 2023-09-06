047be75f50ad56ab4585eb2eb6fb8e615228ac60
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-06 10:33:48 -0300

    refactor: remove duplicated validation logic

--- src/controllers/product/edit-product-controller.ts
+++ src/controllers/product/edit-product-controller.ts
@@ -9,38 +9,42 @@ export class EditProductController {
   ) {}
 
   async handle (request: HttpRequest) {
-    const { id } = request.params
+    try {
+      const { id } = request.params
 
-    const idValidation = z.string().cuid()
+      const idValidation = z.string().cuid()
 
-    const validation = idValidation.safeParse(id)
+      const validation = idValidation.safeParse(id)
 
-    if (!validation.success) {
-      return badRequest(httpError('Product id must be a cuid'))
+      if (!validation.success) {
+        return badRequest(httpError('Product id must be a cuid'))
+      }
+
+      const product = await this.editProduct.execute(id, request.body)
+
+      return ok(product)
+    } catch (err) {
+      const error = err as Error
+
+      if (error.message === 'name cannot be a empty string') {
+        return badRequest(httpError('Name cannot be a empty string'))
+      }
+
+      if (error.message === 'price must be a 0 or positive number') {
+        return badRequest(httpError('Price cannot be a negative number'))
+      }
+
+      if (error.message === 'stock must be a 0 or positive number') {
+        return badRequest(httpError('Stock cannot be a negative number'))
+      }
+
+      if (error.message === 'name must be at least 3 characters long') {
+        return badRequest(httpError('Name must be at least 3 characters long'))
+      }
+
+      if (error.message === 'must pass a value for fields to edit a product: name, price or stock') {
+        return badRequest(httpError('Must pass a value for fields to edit a product: name, price or stock'))
+      }
     }
-
-    if (request.body.name === '') {
-      return badRequest(httpError('Name cannot be a empty string'))
-    }
-
-    if (request.body.price && request.body.price < 0) {
-      return badRequest(httpError('Price cannot be a negative number'))
-    }
-
-    if (request.body.stock && request.body.stock < 0) {
-      return badRequest(httpError('Stock cannot be a negative number'))
-    }
-
-    if (request.body.name && request.body.name.length < 3) {
-      return badRequest(httpError('Name must be at least 3 characters long'))
-    }
-
-    if (!request.body.name && !request.body.price && !request.body.stock) {
-      return badRequest(httpError('Must pass a value for fields to edit a product: name, price or stock'))
-    }
-
-    const product = await this.editProduct.execute(id, request.body)
-
-    return ok(product)
   }
 }
