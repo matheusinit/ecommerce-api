d33d09078684dd79fcee0d2a718aada281faeaf0
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-12 10:25:22 -0300

    feat: ensure when user is not found with given email, then should get an error

--- src/usecases/user/confirmation-email.ts
+++ src/usecases/user/confirmation-email.ts
@@ -1,6 +1,11 @@
 import z from 'zod'
+import { type UserRepository } from '~/data/repositories/protocols/user-repository'
 
 export class ConfirmationEmail {
+  constructor (
+    private readonly userRepository: UserRepository
+  ) {}
+
   async send (email: string) {
     if (!email) {
       throw new Error('Email is required')
@@ -12,6 +17,14 @@ export class ConfirmationEmail {
 
     if (!emailValidation.success) {
       throw new Error('Invalid email was provided does not has the format: john.doe@email.com')
+    }
+
+    const user = await this.userRepository.findByEmail({
+      email
+    })
+
+    if (!user) {
+      throw new Error('User not found with given email')
     }
   }
 }
