06d0599b92b14275fe6b68ad3e8377a9d3cecc95
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-12 10:25:32 -0300

    test: ensure when user is not found with given email, then should get an error

--- src/usecases/user/confirmation-email.spec.ts
+++ src/usecases/user/confirmation-email.spec.ts
@@ -1,9 +1,11 @@
 import { it, describe, expect } from 'vitest'
 import { ConfirmationEmail } from './confirmation-email'
+import { InMemoryUserRepository } from '~/data/repositories/in-memory/in-memory-user-repository'
 
 describe('Send confirmation email', () => {
   it('when email is not provided, then should get an error', async () => {
-    const sut = new ConfirmationEmail()
+    const userRepository = new InMemoryUserRepository()
+    const sut = new ConfirmationEmail(userRepository)
 
     // @ts-expect-error "Pass email as undefined to test case"
     const promise = sut.send()
@@ -12,10 +14,20 @@ describe('Send confirmation email', () => {
   })
 
   it('when an invalid email is provided, then should get an error', async () => {
-    const sut = new ConfirmationEmail()
+    const userRepository = new InMemoryUserRepository()
+    const sut = new ConfirmationEmail(userRepository)
 
     const promise = sut.send('invalid-email')
 
     void expect(promise).rejects.toThrowError('Invalid email was provided does not has the format: john.doe@email.com')
+  })
+
+  it('when a user is not found with given email, then should get an error', async () => {
+    const userRepository = new InMemoryUserRepository()
+    const sut = new ConfirmationEmail(userRepository)
+
+    const promise = sut.send('matheus@email.com')
+
+    void expect(promise).rejects.toThrowError('User not found with given email')
   })
 })
