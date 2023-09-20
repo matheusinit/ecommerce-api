318080e4f531a52d93f59645db3425be87647f94
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-20 08:28:24 -0300

    test: ensure when message is valid, then should create a hash with email and current datetime

--- src/config/mq/email-consumer.spec.ts
+++ src/config/mq/email-consumer.spec.ts
@@ -5,7 +5,8 @@ import { EmailConsumer } from './email-consumer'
 describe('Email consumer', () => {
   it('when method consume is called, then should call user MQ Repository', async () => {
     const inMemoryMQUserRepository = new InMemoryUserMessageQueueRepository()
-    const sut = new EmailConsumer(inMemoryMQUserRepository)
+    const hash = vitest.fn().mockImplementationOnce(async (value: string) => 'hashedValue')
+    const sut = new EmailConsumer(inMemoryMQUserRepository, hash)
     const repositoryMQListenSpy = vitest.spyOn(inMemoryMQUserRepository, 'listen')
 
     await sut.consume()
@@ -15,11 +16,22 @@ describe('Email consumer', () => {
 
   it('when payload is null, then should return null', async () => {
     const inMemoryMQUserRepository = new InMemoryUserMessageQueueRepository()
-    const sut = new EmailConsumer(inMemoryMQUserRepository)
+    const hash = vitest.fn().mockImplementationOnce(async (value: string) => 'hashedValue')
+    const sut = new EmailConsumer(inMemoryMQUserRepository, hash)
     vitest.spyOn(inMemoryMQUserRepository, 'listen').mockImplementationOnce(async () => null)
 
     const result = await sut.consume()
 
     expect(result).toEqual(null)
+  })
+
+  it('when message is valid, then should create a hash with email and current datetime', async () => {
+    const inMemoryMQUserRepository = new InMemoryUserMessageQueueRepository()
+    const hash = vitest.fn().mockImplementationOnce(async (value: string) => 'hashedValue')
+    const sut = new EmailConsumer(inMemoryMQUserRepository, hash)
+
+    await sut.consume()
+
+    expect(hash).toHaveBeenCalledWith(expect.any(String))
   })
 })
