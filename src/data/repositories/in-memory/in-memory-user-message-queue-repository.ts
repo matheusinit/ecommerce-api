1c368800c0e8fcf40140ca6f6ae87685b2496d35
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-19 22:13:16 -0300

    chore: set the type changes for in memory user message queue repository

--- src/data/repositories/in-memory/in-memory-user-message-queue-repository.ts
+++ src/data/repositories/in-memory/in-memory-user-message-queue-repository.ts
@@ -1,10 +1,15 @@
-import { type EmailPayload, type UserMessageQueueRepository } from '../protocols/user-repository-mq'
+import { type MessageQueueResult, type EmailPayload, type UserMessageQueueRepository } from '../protocols/user-repository-mq'
 
 export class InMemoryUserMessageQueueRepository implements UserMessageQueueRepository {
-  private readonly queue: EmailPayload[] = []
+  private readonly queue: string[] = []
 
-  async addEmailTaskToQueue (payload: EmailPayload): Promise<void> {
-    this.queue.push(payload)
+  async addEmailTaskToQueue (email: string): Promise<MessageQueueResult> {
+    this.queue.push(email)
+
+    return {
+      error: false,
+      message: 'Message queue acked'
+    }
   }
 
   async listen (): Promise<EmailPayload | null> {
