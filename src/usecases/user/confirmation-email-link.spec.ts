d09e250dab73f7620a368adfe187cf00c9ce855f
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-05 15:23:25 -0300

    test: ensure when email is provided, then should create a hash with email and current datetime

---
+++ src/usecases/user/confirmation-email-link.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect, vitest } from 'vitest'
+import { ConfirmationEmailLinkImpl } from './confirmation-email-link'
+
+const makeSut = () => {
+  const hash = vitest.fn().mockImplementationOnce(async (value: string) => 'salt:faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26')
+  const sut = new ConfirmationEmailLinkImpl(hash)
+
+  return {
+    hash,
+    sut
+  }
+}
+
+describe('Confirmation Email Link', () => {
+  it('when email is provided, then should create a hash with email and current datetime', async () => {
+    const { sut, hash } = makeSut()
+
+    await sut.create('matheus@email.com')
+
+    expect(hash).toHaveBeenCalledWith(expect.any(String))
+  })
+})
