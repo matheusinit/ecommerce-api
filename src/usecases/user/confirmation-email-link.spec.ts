73b927cc48f9b43938fc46d593b833538272590e
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-05 16:17:16 -0300

    test: ensure when email is provided, then should store token in database

--- src/usecases/user/confirmation-email-link.spec.ts
+++ src/usecases/user/confirmation-email-link.spec.ts
@@ -2,11 +2,17 @@ import { describe, it, expect, vitest } from 'vitest'
 import { ConfirmationEmailLinkImpl } from './confirmation-email-link'
 
 const makeSut = () => {
+  class ConfirmationEmailTokenRepository {
+    async storeToken (email: string, token: string) {}
+  }
+
   const hash = vitest.fn().mockImplementationOnce(async (value: string) => 'salt:faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26')
-  const sut = new ConfirmationEmailLinkImpl(hash)
+  const confirmationEmailTokenRepository = new ConfirmationEmailTokenRepository()
+  const sut = new ConfirmationEmailLinkImpl(hash, confirmationEmailTokenRepository)
 
   return {
     hash,
+    confirmationEmailTokenRepository,
     sut
   }
 }
@@ -26,5 +32,15 @@ describe('Confirmation Email Link', () => {
     const link = await sut.create('matheus@email.com')
 
     expect(link).toMatch(/\/confirmation\?link=[a-z0-9]{128}/gm)
+  })
+
+  it('when email is provided, then should store token in database', async () => {
+    const { sut, confirmationEmailTokenRepository } = makeSut()
+    const spy = vitest.spyOn(confirmationEmailTokenRepository, 'storeToken')
+
+    await sut.create('matheus@email.com')
+
+    const expectedToken = 'faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26'
+    expect(spy).toHaveBeenCalledWith('matheus@email.com', expectedToken)
   })
 })
