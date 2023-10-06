d79bb882102089ae4e8509c54437a463472ea3cc
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-06 14:58:36 -0300

    feat: ensure when valid token provided is not stored in database, then should throw an error

--- src/data/repositories/protocols/confirmation-email-token.ts
+++ src/data/repositories/protocols/confirmation-email-token.ts
@@ -1,3 +1,6 @@
+import { type ConfirmationEmailToken } from '@prisma/client'
+
 export abstract class ConfirmationEmailTokenRepository {
   abstract storeToken (email: string, token: string): Promise<void>
+  abstract getByToken (token: string): Promise<ConfirmationEmailToken | null>
 }
--- src/usecases/user/email.ts
+++ src/usecases/user/email.ts
@@ -1,5 +1,19 @@
+import { z } from 'zod'
+import { type ConfirmationEmailTokenRepository } from '~/data/repositories/protocols/confirmation-email-token'
+
 export class Email {
+  constructor (
+    private readonly confirmationEmailTokenRepository: ConfirmationEmailTokenRepository
+  ) {}
+
   async confirm (token: string) {
-    throw new Error('Invalid token')
+    const tokenSchema = z.string().regex(/^[a-z0-9]{128}$/)
+    const result = tokenSchema.safeParse(token)
+
+    if (!result.success) {
+      throw new Error('Invalid token')
+    }
+
+    throw new Error('Token not found')
   }
 }
