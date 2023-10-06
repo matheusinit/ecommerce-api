09fdb10d62c5889a1cbba9ba4fc724a4db99d92c
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-06 18:03:12 -0300

    test: ensure when invalid token is provided, then should get not found

--- src/controllers/user/confirm-email-controller.test.ts
+++ src/controllers/user/confirm-email-controller.test.ts
@@ -13,4 +13,17 @@ describe('Confirm Email Controller', () => {
     expect(response.statusCode).toBe(400)
     expect(response.body.message).toBe('Invalid token')
   })
+
+  it('when provided token is not found is database, then should get not found', async () => {
+    const validToken = 'faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26'
+
+    const response = await request(app)
+      .post('/v1/users/email-confirmation')
+      .query({
+        token: validToken
+      }).send()
+
+    expect(response.statusCode).toBe(404)
+    expect(response.body.message).toBe('Token not found')
+  })
 })
