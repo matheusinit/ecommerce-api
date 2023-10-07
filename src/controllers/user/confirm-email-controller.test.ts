f35e9a8ded40174f41a73351249ed511ac7aa478
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-07 07:42:01 -0300

    test: when user is already verified, then should get bad request

--- src/controllers/user/confirm-email-controller.test.ts
+++ src/controllers/user/confirm-email-controller.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vitest } from 'vitest'
+import { describe, it, expect, vitest, afterEach } from 'vitest'
 import request from 'supertest'
 import app from '~/app'
 import { PrismaConfirmationEmailTokenRepository } from '~/data/repositories/prisma/prisma-confirmation-email-token-repository'
@@ -7,6 +7,10 @@ import dayjs from 'dayjs'
 import { PrismaUserRepository } from '~/data/repositories/prisma/prisma-user-repository'
 
 describe('Confirm Email Controller', () => {
+  afterEach(async () => {
+    await prisma.user.deleteMany()
+  })
+
   it('when invalid token is provided, then should get bad request', async () => {
     const response = await request(app)
       .post('/v1/users/email-confirmation')
@@ -64,5 +68,28 @@ describe('Confirm Email Controller', () => {
 
     expect(response.statusCode).toBe(400)
     expect(response.body.message).toBe('Token expired')
+  })
+
+  it('when user is already verified, then should get bad request', async () => {
+    const token = 'faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26'
+    const userRepository = new PrismaUserRepository()
+    await userRepository.store({
+      email: 'matheus@email.com',
+      type: 'CUSTOMER',
+      password: 'hashing'
+    })
+    await userRepository.verify('matheus@email.com')
+    const repository = new PrismaConfirmationEmailTokenRepository()
+    await repository.storeToken('matheus@email.com', token)
+
+    const response = await request(app)
+      .post('/v1/users/email-confirmation')
+      .query({
+        token
+      })
+      .send()
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body.message).toBe('User is already verified')
   })
 })
