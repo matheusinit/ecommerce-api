5a45fb60fa5c0b72b955a067ed52c2efd0257710
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-07 09:42:36 -0300

    refactor: move fake email to its own file

--- src/controllers/user/confirm-email-controller.spec.ts
+++ src/controllers/user/confirm-email-controller.spec.ts
@@ -1,15 +1,10 @@
 import { describe, it, expect } from 'vitest'
 import { ConfirmEmailController } from './confirm-email-controller'
 import { type HttpRequest } from '~/infra/protocols/http-request'
+import { FakeEmail } from 'test/fakes/fake-email'
 
 describe('Confirm Email Controller', () => {
   it('when unexpected error is thrown, then should get internal server error', async () => {
-    class FakeEmail {
-      async confirm (token: string): Promise<void> {
-        throw Error('Unexpected error')
-      }
-    }
-
     const email = new FakeEmail()
     const confirmEmailController = new ConfirmEmailController(email)
     const token = 'faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26'
