bbdc911099b1f5c9f9a6af151e3fda160786b351
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-13 10:22:05 -0300

    test: change post method to a more accurate one patch

--- src/controllers/user/confirm-email-controller.test.ts
+++ src/controllers/user/confirm-email-controller.test.ts
@@ -6,14 +6,14 @@ import { prisma } from '~/infra/db'
 import dayjs from 'dayjs'
 import { PrismaUserRepository } from '~/data/repositories/prisma/prisma-user-repository'
 
-describe('Confirm Email Controller', () => {
+describe('PATCH /users/confirmation-email', () => {
   afterEach(async () => {
     await prisma.user.deleteMany()
   })
 
   it('when invalid token is provided, then should get bad request', async () => {
     const response = await request(app)
-      .post('/v1/users/email-confirmation')
+      .patch('/v1/users/email-confirmation')
       .query({
         token: 'invalid-token'
       }).send()
@@ -26,7 +26,7 @@ describe('Confirm Email Controller', () => {
     const validToken = 'faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26'
 
     const response = await request(app)
-      .post('/v1/users/email-confirmation')
+      .patch('/v1/users/email-confirmation')
       .query({
         token: validToken
       }).send()
@@ -60,7 +60,7 @@ describe('Confirm Email Controller', () => {
     await repository.storeToken('matheus@email.com', token)
 
     const response = await request(app)
-      .post('/v1/users/email-confirmation')
+      .patch('/v1/users/email-confirmation')
       .query({
         token
       })
@@ -83,7 +83,7 @@ describe('Confirm Email Controller', () => {
     await repository.storeToken('matheus@email.com', token)
 
     const response = await request(app)
-      .post('/v1/users/email-confirmation')
+      .patch('/v1/users/email-confirmation')
       .query({
         token
       })
@@ -105,7 +105,7 @@ describe('Confirm Email Controller', () => {
     await repository.storeToken('matheus@email.com', token)
 
     const response = await request(app)
-      .post('/v1/users/email-confirmation')
+      .patch('/v1/users/email-confirmation')
       .query({
         token
       })
