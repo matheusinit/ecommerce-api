f6dd92325bf094a2bb01eedd3fef443f15780f14
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-13 11:10:24 -0300

    test: ensure when user is verified, then should get bad request

--- src/controllers/user/send-confirmation-token-controller.test.ts
+++ src/controllers/user/send-confirmation-token-controller.test.ts
@@ -1,11 +1,59 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterEach } from 'vitest'
 import request from 'supertest'
 import app from '~/app'
+import { prisma } from '~/infra/db'
+import { type User } from '~/data/dtos/user'
+import { PrismaUserRepository } from '~/data/repositories/prisma/prisma-user-repository'
+import { PrismaConfirmationEmailTokenRepository } from '~/data/repositories/prisma/prisma-confirmation-email-token-repository'
+import { hash } from '~/utils/hashing'
+import { type Tokens } from '~/data/dtos/auth-tokens'
 
 describe('POST /v1/users/email-confirmation', () => {
+  afterEach(async () => {
+    await prisma.confirmationEmailToken.deleteMany()
+    await prisma.user.deleteMany()
+  })
+
   it('when user is not signed in, then should get unauthorized', async () => {
-    const response = await request(app).post('/v1/users/email-confirmation').send()
+    const response = await request(app).post('/v1/users/email-confirmation').send({
+      email: 'matheus@email.com'
+    })
 
     expect(response.statusCode).toBe(401)
+  })
+
+  it('when user is verified, then should get bad request', async () => {
+    const rawPassword = 'minhasenha1!'
+    const hashedPassword = await hash(rawPassword)
+    const user: User = {
+      name: 'Matheus Oliveira',
+      type: 'STORE-ADMIN',
+      email: 'matheus.oliveira@email.com',
+      password: hashedPassword
+    }
+    const userRepository = new PrismaUserRepository()
+    await userRepository.store(user)
+    const confirmationEmailTokenRepository = new PrismaConfirmationEmailTokenRepository()
+    const token = 'faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26'
+    await confirmationEmailTokenRepository.storeToken(user.email, token)
+    await userRepository.verify(user.email)
+    const { body } = await request(app)
+      .post('/v1/auth')
+      .send({
+        email: user.email,
+        password: rawPassword
+      })
+
+    const tokens: Tokens = body
+
+    const response = await request(app)
+      .post('/v1/users/email-confirmation')
+      .set('Cookie', [`access-token=${tokens.accessToken}`, `refresh-token=${tokens.refreshToken}`])
+      .send({
+        email: user.email
+      })
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body.message).toBe('User is already verified')
   })
 })
