ac83bc45eb6c9d16b7c87e181c31455b3f105835
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-13 11:29:12 -0300

    test: ensure when email is not related to any user, then should get not found

--- src/controllers/user/send-confirmation-token-controller.test.ts
+++ src/controllers/user/send-confirmation-token-controller.test.ts
@@ -27,6 +27,41 @@ describe('POST /v1/users/email-confirmation', () => {
     expect(response.statusCode).toBe(401)
   })
 
+  it('when email is not related to any user, then should get not found', async () => {
+    const rawPassword = 'minhasenha1!'
+    const hashedPassword = await hash(rawPassword)
+    const user: User = {
+      name: 'Matheus Oliveira',
+      type: 'STORE-ADMIN',
+      email: 'matheus.oliveira@email.com',
+      password: hashedPassword
+    }
+    const userRepository = new PrismaUserRepository()
+    await userRepository.store(user)
+    const confirmationEmailTokenRepository = new PrismaConfirmationEmailTokenRepository()
+    const token = 'faa61c5709342a843d3c3e5181474f22b3ad181471faa7c23d6d757bafa3883db473ae0088f727e1402b6c2a823557284742b4eaee94f5fe51af490eb96fdf26'
+    await confirmationEmailTokenRepository.storeToken(user.email, token)
+    const { body } = await request(app)
+      .post('/v1/auth')
+      .send({
+        email: user.email,
+        password: rawPassword
+      })
+
+    const tokens: Tokens = body
+    const email = 'not-existent-email@email.com'
+
+    const response = await request(app)
+      .post('/v1/users/email-confirmation')
+      .set('Cookie', [`access-token=${tokens.accessToken}`, `refresh-token=${tokens.refreshToken}`])
+      .send({
+        email
+      })
+
+    expect(response.statusCode).toBe(404)
+    expect(response.body.message).toBe('User not found with given email')
+  })
+
   it('when user is verified, then should get bad request', async () => {
     const rawPassword = 'minhasenha1!'
     const hashedPassword = await hash(rawPassword)
